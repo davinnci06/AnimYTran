f6f4a9c968d94f3a240e38b10db8c77e6b149546
davjuv <davinnci06@gmail.com>
2023-11-01 21:14:40 -0500

    Initial Activity

--- app/src/main/java/com/example/animytran/MainActivity.java
+++ app/src/main/java/com/example/animytran/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,20 +20,22 @@ public class MainActivity extends AppCompatActivity {
         TextView tV1 = findViewById(R.id.tV1);
         TextView tV2 = findViewById(R.id.tV2);
         Button btn1 = findViewById(R.id.btn1);
-        TextView tv3 = findViewById(R.id.tv3);
+        TextView tV3 = findViewById(R.id.tV3);
+        ImageView iV2 = findViewById(R.id.iV2);
 
         //Debo crear la animación y cargarle lo que hice en fade_out
         Animation anim_out = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
         Animation anim_in = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
         Animation anim_rota = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotation);
         Animation anim_trans = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.translation);
+        Animation anim_scale = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
 
         //Debo iniciar la animación al elemento
         tV1.startAnimation(anim_out);
         tV2.startAnimation(anim_in);
         btn1.startAnimation(anim_rota);
-        tv3.startAnimation(anim_trans);
-
+        tV3.startAnimation(anim_trans);
+        iV2.startAnimation(anim_scale);
 
     }
 }
